f9113ed0abdd00468485ffd46480e2178ce62c80
Dionarley <dionarley@gmail.com>
2025-08-30 17:19:36 -0300

    Create setup_project.sh

---
+++ setup_project.sh
@@ -0,0 +1,6 @@
+git init
+git add setup_project.sh
+git commit -m "Adiciona script de configuração de projeto"
+git branch -M main
+git remote add origin https://github.com/SEU_USUARIO/scripts-publicos.git
+git push -u origin main
