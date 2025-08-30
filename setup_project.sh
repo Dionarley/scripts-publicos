c751f1ef6c68ec06a7fcaa24d94e005b73f7ae50
Dionarley <dionarley@gmail.com>
2025-08-30 17:32:52 -0300

    Update setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -2,5 +2,5 @@ git init
 git add setup_project.sh
 git commit -m "Adiciona script de configuração de projeto"
 git branch -M main
-git remote add origin https://github.com/SEU_USUARIO/scripts-publicos.git
+git remote add origin https://github.com/dionarley/scripts-publicos.git
 git push -u origin main
